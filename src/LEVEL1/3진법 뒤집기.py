056a6778e7eadb5fa14ea07ebaefc8c28b5d252c
LIMHUIWON <59960678+LIMHUIWON@users.noreply.github.com>
2021-02-11 23:52:31 +0900

    Update 3진법 뒤집기.py

--- src/LEVEL1/3진법 뒤집기.py
+++ src/LEVEL1/3진법 뒤집기.py
@@ -1,19 +1,19 @@
 def solution(n):
-    _three = 0
-    i = 0
-    while(n >= 3):
-        _three += (n % 3) * 10**i
-        i += 1
-        n //= 3
-    _three += n*10**i
+  _three = 0
+  i = 0
+  while(n >= 3):
+    _three += (n % 3) * 10**i
+    i += 1
+    n //= 3
+  _three += n*10**i
 
-    rev = int(str(_three)[::-1])
+  rev = int(str(_three)[::-1])
 
-    _ten = 0
-    i = 0
-    while(rev > 0):
-        _ten += rev % 10 * 3**i
-        i += 1
-        rev //= 10
+  _ten = 0
+  i = 0
+  while(rev > 0):
+    _ten += rev % 10 * 3**i
+    i += 1
+    rev //= 10
 
-    return _ten
+  return _ten
