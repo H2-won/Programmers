2e0c80f2f660a1118a54c529920e810c6223a25b
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2022-03-05 00:01:41 +0900

    1 solved
    DP 문제

---
+++ src/LEVEL2/땅따먹기.py
@@ -0,0 +1,10 @@
+def solution(land):
+    for i in range(1, len(land)):
+        for j in range(4):
+            tmp = land[i][j]
+            for k in range(4):
+                if j==k:
+                    continue
+                land[i][j] = max(land[i][j], tmp + land[i-1][k])
+        
+    return max(land[-1])
